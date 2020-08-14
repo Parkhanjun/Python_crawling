278a64ddf7259bc3ce1eca628947c1ba13c08d7b
박한준 <safa65@naver.com>
2020-08-14 11:32:03 +0900

    2020.08.14 naver검색api

--- practice/bs4/03_ api_search.py
+++ practice/bs4/03_ api_search.py
@@ -1,6 +1,6 @@
 import requests
 from urllib.parse import urlparse
 result = requests.get(urlparse("https://openapi.naver.com/v1/search/blog?query=" + "에어컨").geturl(),
-            headers={"X-Naver-Client-Id":"HwQRn2emMFQhUh3R2XvA",
-                     "X-Naver-Client-Secret":"ZDBVXuB3Ui"})
+            headers={"X-Naver-Client-Id":"",
+                     "X-Naver-Client-Secret":""})
 print(result)
