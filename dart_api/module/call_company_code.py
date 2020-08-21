0872a19df5d4f90cf40cdcb08c7955f4f086ae36
박한준 <safa65@naver.com>
2020-08-22 08:52:51 +0900

    2020.08.22 엑셀에서 회사코드 불러오는 단

--- dart_api/module/call_company_code.py
+++ dart_api/module/call_company_code.py
@@ -1,1 +1,16 @@
 # 엑셀에서 회사코드 불러오는 로직 짜야함
+import openpyxl as excel
+
+
+def call_cc():
+    lis = []
+    ex_load = excel.load_workbook('C:/Users/safa6/git/python/Python_crawling/dart_api/excel/stock.xlsx')
+    sheet = ex_load['Sheet1']
+    for row in sheet.rows:
+        lis.append(row[0].value)
+
+    return lis
+
+
+if __name__ == "__main__":
+    call_cc()
