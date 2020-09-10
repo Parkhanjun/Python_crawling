d1bff4410da73280c92d4855e585b4c02e54688d
박한준 <safa65@naver.com>
2020-09-10 13:11:28 +0900

    2020.09.10 load and save in excel

--- dart_api/module/call_excel.py
+++ dart_api/module/call_excel.py
@@ -1,6 +1,5 @@
 import openpyxl as excel
 import pandas as pd
-import numpy as np
 
 
 def call_cc():
@@ -9,21 +8,18 @@ def call_cc():
     sheet = ex_load['Sheet1']
     for row in sheet.rows:
         lis.append(row[0].value)
-
     return lis
 
 
 def save_stock_value(open):
-    list = []
-    for item in open:
-        list.append(item)
-    pan1 = pd.DataFrame(list[0])
-    pan2 = pd.DataFrame(list[1])
-    pc = pd.concat([pan1,pan2], axis=1)
-    panda = pd.DataFrame(pc)
-    panda.to_excel('C:/Users/safa6/git/python/Python_crawling/dart_api/excel/test.xlsx',
-                   sheet_name='Sheet1',
-                   )
+    print(open)
+    file_name = open['a']
+    open.pop('a')
+    pandas = pd.DataFrame(open)
+    pandas.to_excel(f'C:/Users/safa6/git/python/Python_crawling/dart_api/excel/{file_name}.xlsx',
+               sheet_name='Sheet1',
+               )
+
 
 
 if __name__ == "__main__":
